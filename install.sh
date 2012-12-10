e5caa3b716c19b5bf8a40e245a048872d78df4e9
Sergio Rabiela <srabiela@enova.com>
2012-12-10 15:26:15 +0000

    added gemrc

--- install.sh
+++ install.sh
@@ -14,3 +14,4 @@ ln -sf  $HOME/.dotfiles/bashrc        ~/.bashrc
 ln -sf  $HOME/.dotfiles/screenrc      ~/.screenrc
 ln -sf  $HOME/.dotfiles/inputrc       ~/.inputrc
 ln -sf  $HOME/.dotfiles/editrc        ~/.editrc
+ln -sf  $HOME/.dotfiles/gemrc        ~/.gemrc
