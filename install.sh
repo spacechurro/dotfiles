01cb29babd14d5572f7c6da46cde8ec7466c4e92
Sergio Rabiela <srabiela@enova.com>
2012-12-28 21:24:02 +0000

    cleaning house and adding vimrc

--- install.sh
+++ install.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-#for file in vimrc bashrc screenrc inputrc editrc
-#do
-#  cmd="ln -sf  $HOME/.dotfiles/$file ~/.$file"
-#  echo "$cmd"
-#  `$cmd`
-#  #ln -sf  $HOME/.dotfiles/bashrc        ~/.bashrc
-#  #ln -sf  $HOME/.dotfiles/screenrc      ~/.screenrc
-#  #ln -sf  $HOME/.dotfiles/inputrc       ~/.inputrc
-#  #ln -sf  $HOME/.dotfiles/editrc        ~/.editrc
-#done
 ln -sf  $HOME/.dotfiles/bashrc        ~/.bashrc
 ln -sf  $HOME/.dotfiles/screenrc      ~/.screenrc
 ln -sf  $HOME/.dotfiles/inputrc       ~/.inputrc
 ln -sf  $HOME/.dotfiles/editrc        ~/.editrc
 ln -sf  $HOME/.dotfiles/gemrc        ~/.gemrc
+ln -sf  $HOME/.dotfiles/vimrc        ~/.vimrc
