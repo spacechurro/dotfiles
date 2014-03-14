1194d0e33c8ee0169cae7659e74b6a8959ee1737
Sergio Rabiela <sergio@noesbueno.com>
2014-03-13 21:21:03 -0500

    tmux config

--- install.sh
+++ install.sh
@@ -8,3 +8,4 @@ ln -sf  $HOME/.dotfiles/gemrc        ~/.gemrc
 ln -sf  $HOME/.dotfiles/vimrc        ~/.vimrc
 ln -sf  $HOME/.dotfiles/irbrc        ~/.irbrc
 ln -sf  $HOME/.dotfiles/bash_profile        ~/.bash_profile
+ln -sf  $HOME/.dotfiles/tmux.conf        ~/.tmux.conf
