d0411e62ed6169f05983779f37937eb5bfbc6160
Sergio Rabiela <sergiorabiela@gmail.com>
2014-06-09 13:38:23 -0500

    pow support w/ rbenv

--- install.sh
+++ install.sh
@@ -9,3 +9,4 @@ ln -sf  $HOME/.dotfiles/vimrc        ~/.vimrc
 ln -sf  $HOME/.dotfiles/irbrc        ~/.irbrc
 ln -sf  $HOME/.dotfiles/bash_profile        ~/.bash_profile
 ln -sf  $HOME/.dotfiles/tmux.conf        ~/.tmux.conf
+ln -sf  $HOME/.dotfiles/powconfig        ~/.powconfig
